3e25ad1e2b9a235c704238341e9baaf63f23b1cc
Gui-alme <guilherme.almeida@sptech.school>
2022-12-02 20:41:52 -0300

    model da medida com instrucoes da azure add

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,14 +5,9 @@ function listarPorEmpresa(idEmpresa) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
+        instrucaoSql = `SELECT TOP 12 count(*) as status_falta, DATE_FORMAT(dtPrateleira,'%H:00') as hora from dados_sensor
+        JOIN Prateleira on idPrateleira = fkEmpresa
+        where statusPrateleira <> 3 and fkEmpresa = ${idEmpresa} group by day(dtPrateleira), hour(dtPrateleira) order by idDado desc;`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
 
         instrucaoSql = `	SELECT count(*) as status_falta, DATE_FORMAT(dtPrateleira,'%H:00') as hora from dados_sensor
@@ -62,13 +57,15 @@ function obterPie(idEmpresa) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
+        instrucaoSql = `SELECT (SELECT DISTINCT ROUND ((SUM(statusPrateleira) / (44 * 3) * 100)) FROM (SELECT TOP 44 ds.statusPrateleira FROM dados_sensor ds 
+            JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+            JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa}
+            ORDER BY ds.idDado DESC) as wip1 ) as abastecido,
+                                    (SELECT DISTINCT ROUND ((100 - (SUM(statusPrateleira) / (44 * 3) * 100))) FROM 
+                                    (SELECT TOP 44 ds.statusPrateleira FROM dados_sensor ds 
+            JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+            JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa}
+            ORDER BY ds.idDado DESC) AS wip2)  as nao_abastecido;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `SELECT (SELECT DISTINCT ROUND ((SUM(statusPrateleira) / (44 * 3) * 100)) FROM (SELECT ds.statusPrateleira FROM dados_sensor ds 
@@ -94,13 +91,31 @@ function obterPolar(idEmpresa) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
+        instrucaoSql = `SELECT
+        (SELECT (SELECT (24 - SUM(statusPrateleira)) FROM (SELECT TOP 8 ds.statusPrateleira FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Frios e congelados'
+        ORDER BY ds.idDado DESC) as empresa_dados)) faltas_frios,
+        
+        (SELECT (SELECT (30 - SUM(statusPrateleira)) FROM (SELECT TOP 10 ds.statusPrateleira FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Mercearia'
+        ORDER BY ds.idDado DESC) as empresa_dados)) faltas_mercearia,
+        
+        (SELECT (SELECT (30 - SUM(statusPrateleira)) FROM (SELECT TOP 10 ds.statusPrateleira FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Hortifruti'
+        ORDER BY ds.idDado DESC) as empresa_dados)) faltas_hortifruti,
+        
+        (SELECT (SELECT (21 - SUM(statusPrateleira)) FROM (SELECT TOP 7 ds.statusPrateleira FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Cuidados Pessoais'
+        ORDER BY ds.idDado DESC) as empresa_dados)) faltas_cuidados,
+        
+        (SELECT (SELECT (30 - SUM(statusPrateleira)) FROM (SELECT TOP 10 ds.statusPrateleira FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Bebidas'
+        ORDER BY ds.idDado DESC) as empresa_dados)) faltas_bebidas;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `SELECT
@@ -142,13 +157,31 @@ function obterDonut(idEmpresa) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
+        instrucaoSql = `SELECT 
+        (SELECT (SELECT ((COUNT(ds.statusPrateleira) * 3) - (SUM(ds.statusPrateleira))) FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa
+        WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Frios e congelados' AND MONTH(ds.dtPrateleira) = MONTH(curdate())) AS wip_faltas_mes_frios) faltas_mes_frios,
+        
+        (SELECT (SELECT ((COUNT(ds.statusPrateleira) * 3) - (SUM(ds.statusPrateleira))) FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa
+        WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Mercearia' AND MONTH(ds.dtPrateleira) = MONTH(curdate())) AS wip_faltas_mes_mercearia) faltas_mes_mercearia,
+        
+        (SELECT (SELECT ((COUNT(ds.statusPrateleira) * 3) - (SUM(ds.statusPrateleira))) FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa
+        WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Hortifruti' AND MONTH(ds.dtPrateleira) = MONTH(curdate())) AS wip_faltas_mes_hortifruti) faltas_mes_hortifruti,
+        
+        (SELECT (SELECT ((COUNT(ds.statusPrateleira) * 3) - (SUM(ds.statusPrateleira))) FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa
+        WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Cuidados Pessoais' AND MONTH(ds.dtPrateleira) = MONTH(curdate())) AS wip_faltas_mes_cuidados) faltas_mes_cuidados,
+        
+        (SELECT (SELECT ((COUNT(ds.statusPrateleira) * 3) - (SUM(ds.statusPrateleira))) FROM dados_sensor ds 
+        JOIN prateleira prat ON ds.fkPrateleira = prat.idPrateleira
+        JOIN empresa e ON prat.fkEmpresa = e.idEmpresa
+        WHERE e.idEmpresa = ${idEmpresa} AND prat.setor = 'Bebidas' AND MONTH(ds.dtPrateleira) = MONTH(curdate())) AS wip_faltas_mes_bebidas) faltas_mes_bebidas;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `SELECT 
