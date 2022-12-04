44de3d5de8a5a93dd550d5fee9750c1419673b7e
gabs-mvb <gabriel.boas@sptech.school>
2022-12-04 19:54:48 -0300

    'sidebar'

--- site/public/js/perfil-config.js
+++ site/public/js/perfil-config.js
@@ -277,18 +277,56 @@ botao.addEventListener('click', () => {
   selecionarFoto.click();
 })
 
-selecionarFoto.addEventListener('change', (event) => {
+selecionarFoto.addEventListener('change', (e) => {
   
   if(selecionarFoto.files.length <= 0) {
     return;
   }
 
     var ler = new FileReader();
-
+    var size = selecionarFoto.files[0].size;
     ler.onload = () => {
-      imagem.src = ler.result;
+      if(size < 90000) { //1MB         
+        sessionStorage.PERFIL_IMAGEM = ler.result;
+        imagem.src = ler.result;
+        adicionarImg();
+      } else {           
+        alert('Não permitido, excedeu o limite de 1MB'); //Acima do limite
+        selecionarFoto.value = ""; //Limpa o campo          
+      }
+      e.preventDefault();
+
+      
+      
     }
 
     ler.readAsDataURL(selecionarFoto.files[0]);
 
 });
+
+function adicionarImg() {
+
+  fetch(`/usuarios/adicionarImg/${sessionStorage.ID_PERFIL}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      idPerfil: sessionStorage.ID_PERFIL,
+      ImagemServer: sessionStorage.PERFIL_IMAGEM
+    })
+  }).then(function (resposta) {
+    
+    if (resposta.ok) {
+      window.alert("Foto atualizada com sucesso");
+      window.location.reload();
+      
+    } else if (resposta.status == 404) {
+      window.alert("Deu 404!");
+    } else {
+      throw ("Houve um erro ao tentar atualizar a foto! Código da resposta: " + resposta.status);
+    }
+  }).catch(function (resposta) {
+    console.log(`#ERRO: ${resposta}`);
+  })
+}
