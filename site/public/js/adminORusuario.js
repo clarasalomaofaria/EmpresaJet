72a683a69b2f52013a41813cb17a47d24a2c3a2d
CaiqueGomesdaSilva <caique.gsilva@sptech.school>
2022-12-04 13:02:42 -0300

    teste

--- site/public/js/adminORusuario.js
+++ site/public/js/adminORusuario.js
@@ -1,8 +1,105 @@
 function verificarFuncao() {
-  if (sessionStorage.FUNCAO == "Administrador") {
-    var containerGlobal = document.getElementById("container-global");
+  if (sessionStorage.FUNCAO == "Administrador" && sessionStorage.ID_EMPRESA == "null") {
+
+      var containerGlobal = document.getElementById("container-global");
+
+    containerGlobal.innerHTML = `<div id="titulo_2_empresa" class="titulo-2">
+    <h3 id="titulo_div_empresa">Cadastre sua empresa</h3>
+    <p id="subtitulo_div_empresa">
+      Por favor, insira os dados da empresa que você deseja cadastrar
+    </p>
+  </div>
+
+  <div class="container-pai">
+    <div id="container_filho_empresa" class="container-filho">
+      <div id="nome_empresa_box" class="estilo-box">
+        <p id="nome_empresa_cadastro">Nome da Empresa*</p>
+        <input class="input-empresa" id="inputEmpresa" placeholder="Ex: Nubank" onblur="validarEmpresa()"
+          required />
+      </div>
+
+      <div id="cnpj_empresa_box" class="estilo-box">
+        <p id="cnpj_empresa_cadastro">CNPJ*</p>
+        <input class="input-empresa" id="inputCNPJ" placeholder="Ex: 69443724000106" onblur="validarCNPJ()"
+          required />
+      </div>
+
+      <div id="logradouro_empresa_box" class="estilo-box">
+        <p id="logradouro_empresa_cadastro">Logradouro*</p>
+        <input class="input-empresa" id="inputLogradouro" placeholder="Ex: Rua Lago Verde"
+          onblur="validarLogradouro()" required />
+      </div>
+
+      <div class="estilo-box-2">
+        <div id="cidade_empresa_box" class="cidade">
+          <p id="cidade_empresa_cadastro">Cidade*</p>
+          <br />
+          <input class="input-empresa" id="inputCidade" placeholder="Ex: Boa Vista" onblur="validarCidade()" />
+        </div>
+
+        <div id="estado_empresa_box" class="estado">
+          <p id="estado_empresa_cadastro">UF*</p>
+          <br />
+          <select class="select-estado" id="inputUF" name="estado" onclick="validarUF()">
+            <option value="none"></option>
+            <option value="AC">Acre</option>
+            <option value="AL">Alagoas</option>
+            <option value="AP">Amapá</option>
+            <option value="AM">Amazonas</option>
+            <option value="BA">Bahia</option>
+            <option value="CE">Ceará</option>
+            <option value="DF">Distrito Federal</option>
+            <option value="ES">Espírito Santo</option>
+            <option value="GO">Goiás</option>
+            <option value="MA">Maranhão</option>
+            <option value="MT">Mato Grosso</option>
+            <option value="MS">Mato Grosso do Sul</option>
+            <option value="MG">Minas Gerais</option>
+            <option value="PA">Pará</option>
+            <option value="PB">Paraíba</option>
+            <option value="PR">Paraná</option>
+            <option value="PE">Pernambuco</option>
+            <option value="PI">Piauí</option>
+            <option value="RJ">Rio de Janeiro</option>
+            <option value="RN">Rio Grande do Norte</option>
+            <option value="RS">Rio Grande do Sul</option>
+            <option value="RO">Rondônia</option>
+            <option value="RR">Roraima</option>
+            <option value="SC">Santa Catarina</option>
+            <option value="SP">São Paulo</option>
+            <option value="SE">Sergipe</option>
+            <option value="TO">Tocantins</option>
+            <option value="EX">Estrangeiro</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-filho-2">
+      <div id="cep_empresa_box" class="estilo-box">
+        <p id="cep_empresa_cadastro">CEP*</p>
+        <input class="input-empresa" id="inputCEP" placeholder="Ex: 69313175" onblur="validarCEP()" required />
+      </div>
+
+      <div id="bairro_empresa_box" class="estilo-box">
+        <p id="bairro_empresa_cadastro">Bairro</p>
+        <input class="input-empresa" id="inputBairro" placeholder="Ex: Jóquei Clube" onblur="validarBairro()" />
+      </div>
+
+      <div id="complemento_empresa_box" class="estilo-box">
+        <p id="complemento_empresa_cadastro">Complemento</p>
+        <input class="input-empresa active" id="inputComplemento" placeholder="Empresa 1"
+          onblur="validarComplemento()" />
+      </div>
+    </div>
+  </div>
+
+  <button id="btn_cadastrar_empresa" class="botao-registro" onclick="cadastrarEmpresa()">
+    CADASTRAR
+  </button>`
 
     // ------------------ TITULO -----------------------------
+
     var divisoria = document.createElement("hr");
     var divTituloFunc = document.createElement("div");
     var titulo = document.createElement("h3");
