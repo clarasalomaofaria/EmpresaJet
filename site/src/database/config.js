4b33e254eddb440402e844133aba61b74f52f3cc
Calebe-Marques <calebe.reboucas@sptech.school>
2022-12-05 14:33:34 -0300

    Documentação atualizada

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "empresajet",
-    user: "admin",
-    password: "admin123",
+    user: "root",
+    password: "#Gf49279450816",
 };
 // user: "admin",
 //     password: "admin123",
